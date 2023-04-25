d0618bdaa7bfb86b1887ac3c70e7966c701e8b70
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-04-25 18:28:49 +0200

    Add an id without using key as a prop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import Heading from "./Header.jsx";
 function createCard(contact){
   return (
     <Card 
+    id={contact.id}
     key={contact.id}
     name={contact.name}
     img={contact.imgURL}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Avatar from "./Avatar.jsx"
 import Detail from "./Detail.jsx";
-
 function Card(props){
     return(<div>
         <div className="card">
           <div className="top">
+          <p>{props.id}</p>
             <h2 className="name">{props.name}</h2>
             <Avatar img={props.img}/>
           </div>
