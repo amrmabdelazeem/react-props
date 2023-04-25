0013a07b4fa13842895d3a74bd6706586618cbb1
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-04-25 18:11:37 +0200

    Use map

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,25 @@ import Card from "./Card.jsx";
 import contacts from "../contacts";
 import Heading from "./Header.jsx";
 
+function createCard(contact){
+  return (
+    <Card 
+    key={contact.id}
+    name={contact.name}
+    img={contact.imgURL}
+    tel={contact.phone}
+    email={contact.email}  />
+  );
+}
 
 function App() {
   return (<div>
   <Heading/>
-  <Card name={contacts[0].name} img={contacts[0].imgURL} tel={contacts[0].phone} email={contacts[0].email}/>
+  {contacts.map(createCard)}
+  {/* <Card name={contacts[0].name} img={contacts[0].imgURL} tel={contacts[0].phone} email={contacts[0].email}/>
 
   <Card name={contacts[1].name} img={contacts[1].imgURL} tel={contacts[1].phone} email={contacts[1].email}/>
-  <Card name={contacts[2].name} img={contacts[2].imgURL} tel={contacts[2].phone} email={contacts[2].email}/>
+  <Card name={contacts[2].name} img={contacts[2].imgURL} tel={contacts[2].phone} email={contacts[2].email}/> */}
   </div>
   );
 }
