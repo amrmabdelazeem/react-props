f86262524813c4bfaffeba78a0fb2155001f8e4e
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-04-25 02:38:42 +0200

    Create Heading component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Card from "./Card.jsx";
 import contacts from "../contacts";
+import Heading from "./Header.jsx";
 
 
 function App() {
   return (<div>
+  <Heading/>
     <Card name="Beyonce" img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
     tel="+123 456 789" email="b@beyonce.com"
   />
