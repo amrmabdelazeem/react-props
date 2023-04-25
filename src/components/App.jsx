792901ff64c9978b2b5053ad31698425aa246930
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-04-25 02:35:09 +0200

    Add new reusable cards and populate with contacts.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
-import contacts from "../contacts";
 import Card from "./Card.jsx";
+import contacts from "../contacts";
+
 
 function App() {
-  return <Card/>
+  return (<div>
+    <Card name="Beyonce" img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+    tel="+123 456 789" email="b@beyonce.com"
+  />
+  <Card name={contacts[1].name} img={contacts[1].imgURL} tel={contacts[1].phone} email={contacts[1].email}/>
+  <Card name={contacts[2].name} img={contacts[2].imgURL} tel={contacts[2].phone} email={contacts[2].email}/>
+  </div>
+  );
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,24 @@
 import React from "react";
-import contacts from "../contacts";
 
 
-function Card(){
-    return 
-    <div>
-      <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2 className="name"></h2>
-          <img className="circle-img"
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
+function Card(props){
+    return(<div>
+        <h1 className="heading">My Contacts</h1>
+        <div className="card">
+          <div className="top">
+            <h2 className="name">{props.name}</h2>
+            <img className="circle-img"
+              src={props.img}
+              alt="avatar_img"
+            />
+          </div>
+          <div className="bottom">
+            <p className="info">{props.tel}</p>
+            <p className="info">{props.email}</p>
+          </div>
         </div>
-        <div className="bottom">
-          <p className="info">+123 456 789</p>
-          <p className="info">b@beyonce.com</p>
-        </div>
-      </div>
-    </div>
+      </div>);
+
 }
 
 export default Card;
